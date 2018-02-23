6fc0860f0958d1483c0277c649d8fb16af6024d6
StanimirDim <webdev@netswitch.co.uk>
2018-02-23 16:49:14 +0200

    puts and print

--- splat_operator.rb
+++ splat_operator.rb
@@ -15,5 +15,67 @@ end
 
 puts sum('1','2',34,'3','3','3','3','4',3)
 
+
+# puts and print gives different values in console!!!
+# puts adds a new line (after each argument); print does not. That's all.
+
+
+puts 
+puts 
+
+
+#nil
+j = *[]
+
+puts j
+p j
+print j
+
+puts 
+puts 
+
+
+k = *[1,3]
+
+puts k
+p k
+print k
+
+
+puts 
+puts 
+
+
+#[1,3]
+j = *1,3
+p j
+
+#[1,3]
 j = *[1,3]
-puts j.class
+print j
+
+print $,
+print $_
+print $\
+
+=begin
+[1, 2, 3]
+[]
+56
+[]
+[1, 3]
+[1, 3]
+
+
+=end
+
+puts $,
+puts $_
+puts $\
+=begin
+[1, 2, 3]
+[]
+56
+[]
+[1, 3]
+=end
