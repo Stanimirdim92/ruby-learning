b642e083697aded3b0383404602e73d677018c0a
StanimirDImInc <webdev@increate.co.uk>
2018-04-14 12:40:01 +0300

    style docs index

--- cabinet/app/models/doc.rb
+++ cabinet/app/models/doc.rb
@@ -1,9 +1,9 @@
 class Doc < ApplicationRecord
   belongs_to :user
 
-  def self.find_docs(user_id)
-    if Doc.exists?(user_id: user_id)
-      Doc.where(user_id: user_id).order('id DESC')
+  def self.find_docs(user)
+    if Doc.exists?(user_id: user)
+      Doc.where(user_id: user).order('id DESC')
     end
 
     Doc.all.order('id DESC')
